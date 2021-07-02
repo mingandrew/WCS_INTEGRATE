4c29671a8dacb5ce689202ea56114509ce5a9d33
KYLE <33379105+KYLEMYWORLD@users.noreply.github.com>
2021-07-02 09:39:00 +0800

    【绑定运输车在储砖轨道绑定库存修复轨道中间绑定库存问题】

--- module/other/CalData.cs
+++ module/other/CalData.cs
@@ -1,9 +1,12 @@
-﻿namespace module.other
+﻿using module.goods;
+
+namespace module.other
 {
     public class CalData
     {
         public uint id { set; get; }
         public ushort s_data { set; get; }
         public ushort s_location { set; get; }
+        public Stock s_stock { set; get; }
     }
 }
