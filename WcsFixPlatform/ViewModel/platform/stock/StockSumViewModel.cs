7a30e1a450ff3ed60327d60888bc4e21a31599e5
KYLE <33379105+KYLEMYWORLD@users.noreply.github.com>
2021-08-02 20:25:41 +0800

    【统计添加轨道库存查询，上砖记录查询】

--- WcsFixPlatform/ViewModel/platform/stock/StockSumViewModel.cs
+++ WcsFixPlatform/ViewModel/platform/stock/StockSumViewModel.cs
@@ -4,6 +4,7 @@ using enums.track;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
 using GalaSoft.MvvmLight.Views;
+using HandyControl.Controls;
 using Microsoft.Win32;
 using module.goods;
 using module.msg;
@@ -29,8 +30,14 @@ namespace wcs.ViewModel
 
         public StockSumViewModel() : base("StockSum")
         {
+            STARTDATE = DateTime.Now;
+            STOPDATE = DateTime.Now;
+
             GoodSumList = new ObservableCollection<StockSumView>();
             TrackSumList = new ObservableCollection<StockSumView>();
+
+            TrackStockSearchList = new ObservableCollection<StockSumView>();
+            ConsumeStockSearchList = new ObservableCollection<StockSumView>();
 
             InitAreaRadio();
 
@@ -57,6 +64,13 @@ namespace wcs.ViewModel
         private ushort filterlineid = 0;
         private StockSumView trackselected;
 
+        #region[轨道时间过滤]
+
+        private DateTime lastquerytime;
+        private DateTime? startdate;
+        private DateTime? stopdate;
+
+        #endregion
         #endregion
 
         #region[属性]
@@ -72,11 +86,10 @@ namespace wcs.ViewModel
             set => Set(ref _arearadio, value);
         }
 
-        public bool ShowTrack
-        {
-            get => showtrack;
-            set => Set(ref showtrack, value);
-        }
+        /// <summary>
+        /// 当前选项卡名称
+        /// </summary>
+        private string Now_Tag_Name { set; get; }
 
         public StockSumView TrackSelected
         {
@@ -87,16 +100,109 @@ namespace wcs.ViewModel
         public ICollectionView GoodSumListView { set; get; }
         public ICollectionView TrackSumListView { set; get; }
 
+        public ObservableCollection<StockSumView> TrackStockSearchList { set; get; }
+        public ObservableCollection<StockSumView> ConsumeStockSearchList { set; get; }
+
+        #region[轨道过滤]
+
+
+        public DateTime? STARTDATE
+        {
+            get => startdate;
+            set => Set(ref startdate, value);
+        }
+
+        public DateTime? STOPDATE
+        {
+            get => stopdate;
+            set => Set(ref stopdate, value);
+        }
+
+        #endregion
         #endregion
 
         #region[命令]        
         public RelayCommand<RoutedEventArgs> CheckRadioBtnCmd => new Lazy<RelayCommand<RoutedEventArgs>>(() => new RelayCommand<RoutedEventArgs>(CheckRadioBtn)).Value;
         public RelayCommand<RoutedEventArgs> CheckTypeRadioBtnCmd => new Lazy<RelayCommand<RoutedEventArgs>>(() => new RelayCommand<RoutedEventArgs>(CheckTypeRadioBtn)).Value;
         public RelayCommand<string> StockSumActionCmd => new Lazy<RelayCommand<string>>(() => new RelayCommand<string>(StockSumAction)).Value;
-        public RelayCommand<RoutedEventArgs> ShowGoodsOrTrackCmd => new Lazy<RelayCommand<RoutedEventArgs>>(() => new RelayCommand<RoutedEventArgs>(ShowGoodsOrTrack)).Value;
         public RelayCommand<StockSumView> ChangeGoodCmd => new Lazy<RelayCommand<StockSumView>>(() => new RelayCommand<StockSumView>(ChangeGood)).Value;
 
         public RelayCommand ExportSumViewCmd => new Lazy<RelayCommand>(() => new RelayCommand(ExportSumView)).Value;
+
+        public RelayCommand<RoutedEventArgs> TabSelectedCmd => new Lazy<RelayCommand<RoutedEventArgs>>(() => new RelayCommand<RoutedEventArgs>(TabSelected)).Value;
+
+        public RelayCommand<string> SetFilterCmd => new Lazy<RelayCommand<string>>(() => new RelayCommand<string>(SetFilter)).Value;
+
+        private void SetFilter(string tag)
+        {
+            if ((DateTime.Now - lastquerytime).TotalMilliseconds < 800)
+            {
+                Growl.Warning("请不要频繁刷新!");
+                return;
+            }
+
+            lastquerytime = DateTime.Now;
+
+            if (STARTDATE == null)
+            {
+                Growl.Warning("请选择开始时间");
+                return;
+            }
+
+            if (STOPDATE == null)
+            {
+                Growl.Warning("请选择结束时间");
+                return;
+            }
+
+            if (int.TryParse(tag, out int type))
+            {
+                switch (type)
+                {
+                    case 1:
+                        break;
+
+                    case 2://今天8-18
+                        DateTime today = DateTime.Now;
+                        STARTDATE = new DateTime(today.Year, today.Month, today.Day, 6, 00, 00);
+                        STOPDATE = new DateTime(today.Year, today.Month, today.Day, 18, 00, 000);
+                        break;
+
+                    case 3://昨天18-今天6
+                        today = DateTime.Now;
+                        today = today.AddDays(-1);
+                        STARTDATE = new DateTime(today.Year, today.Month, today.Day, 18, 00, 00);
+
+                        today = DateTime.Now;
+                        STOPDATE = new DateTime(today.Year, today.Month, today.Day, 06, 00, 00);
+                        break;
+                }
+            }
+
+            if (STARTDATE is DateTime start && STOPDATE is DateTime stop)
+            {
+                if ((stop - start).TotalDays > 60)
+                {
+                    Growl.Warning("查询日期不能超过两周");
+                    return;
+                }
+            }
+
+            switch (Now_Tag_Name)
+            {
+                case "inlog":
+                    QueryTrackInLog();
+                    break;
+                case "outlog":
+                    QueryTrackOutLog();
+                    break;
+                default:
+                    break;
+            }
+        }
+        #endregion
+
+        #region[方法]
 
         private void StockSumAction(string tag)
         {
@@ -111,9 +217,6 @@ namespace wcs.ViewModel
             }
         }
 
-        #endregion
-
-        #region[方法]
         private void CheckIsSingle()
         {
             if (PubMaster.Area.IsSingleAreaLine(out uint areaid, out ushort lineid))
@@ -167,7 +270,6 @@ namespace wcs.ViewModel
             }
             return true;
         }
-
 
         private void StockSumeUpdate(MsgAction msg)
         {
@@ -309,17 +411,7 @@ namespace wcs.ViewModel
         {
             if (args.OriginalSource is RadioButton btn)
             {
-                switch (btn.Tag)
-                {
-                    case "goods":
-                        ShowTrack = false;
-                        break;
-                    case "track":
-                        ShowTrack = true;
-                        break;
-                    default:
-                        break;
-                }
+                Now_Tag_Name = btn.Tag.ToString();
             }
         }
 
@@ -330,30 +422,80 @@ namespace wcs.ViewModel
 
         private void ExportSumView()
         {
-            SaveFileDialog saveFileDialog = ExcelTool.createSaveFileDialog(!ShowTrack ? "品种分类-库存统计-" : "轨道分类-库存统计-");
+            string tile = string.Empty;
+            switch (Now_Tag_Name)
+            {
+                case "goods":
+                    tile = "品种分类-库存统计-";
+                    break;
+                case "track":
+                    tile = "轨道分类-库存统计-";
+                    break;
+                case "inlog":
+                    tile = "库存统计-";
+                    break;
+                case "outlog":
+                    tile = "出库记录统计-";
+                    break;
+                default:
+                    tile = "未命名统计-";
+                    break;
+            }
+            SaveFileDialog saveFileDialog = ExcelTool.createSaveFileDialog(tile);
 
             bool? result = saveFileDialog.ShowDialog();
             if (result == null || result == false)
             {
                 return;
             }
-
-            string sql = "select `g`.`info` AS `品种`,min(`t`.`produce_time`) AS `生产时间`,count(`t`.`id`) AS `车数`,sum(`t`.`stack`) AS `垛数`,sum(`t`.`pieces`) AS `片数`,`a`.`name` AS `区域`," +
+            string sql = string.Empty;
+            switch (Now_Tag_Name)
+            {
+                case "goods":
+                    sql = "select `g`.`info` AS `品种`,min(`t`.`produce_time`) AS `生产时间`,count(`t`.`id`) AS `车数`,sum(`t`.`stack`) AS `垛数`,sum(`t`.`pieces`) AS `片数`,`a`.`name` AS `区域`," +
                     "(select `line`.`name` from (`track` join `line`) where ((`track`.`id` = `t`.`track_id`) and (`track`.`line` = `line`.`line`))) AS `线` " +
                     "from (((`stock` `t` join `goods` `g`) join `track` `tt`) join `area` `a`) " +
                     "where ((`t`.`track_type` in (2,3,4)) and (`t`.`goods_id` = `g`.`id`) and (`t`.`track_id` = `tt`.`id`) and (`a`.`id` = `t`.`area`)) " +
                     "group by `t`.`goods_id` " +
                     "order by `t`.`area`,`t`.`goods_id`,`t`.`produce_time`";
-            if (ShowTrack)
-            {
-                sql = "select `tt`.`name` AS `轨道`,`g`.`info` AS `品种`,min(`t`.`produce_time`) AS `生产时间`,count(`t`.`id`) AS `车数`,sum(`t`.`stack`) AS `垛数`,sum(`t`.`pieces`) AS `片数`," +
-                "`a`.`name` AS `区域`,(select `line`.`name` from (`track` join `line`) where ((`track`.`id` = `t`.`track_id`) and (`track`.`line` = `line`.`line`))) AS `线` " +
-                "from (((`stock` `t` join `goods` `g`) join `track` `tt`) join `area` `a`) " +
-                "where ((`t`.`track_type` in (2,3,4)) and (`t`.`goods_id` = `g`.`id`) and (`t`.`track_id` = `tt`.`id`) and (`a`.`id` = `t`.`area`)) " +
-                "group by `t`.`goods_id`,`t`.`track_id` " +
-                "order by `t`.`area`,`t`.`goods_id`,`t`.`produce_time`,`t`.`track_id`";
+                    break;
+                case "track":
+                    sql = "select `tt`.`name` AS `轨道`,`g`.`info` AS `品种`,min(`t`.`produce_time`) AS `生产时间`,count(`t`.`id`) AS `车数`,sum(`t`.`stack`) AS `垛数`,sum(`t`.`pieces`) AS `片数`," +
+                    "`a`.`name` AS `区域`,(select `line`.`name` from (`track` join `line`) where ((`track`.`id` = `t`.`track_id`) and (`track`.`line` = `line`.`line`))) AS `线` " +
+                    "from (((`stock` `t` join `goods` `g`) join `track` `tt`) join `area` `a`) " +
+                    "where ((`t`.`track_type` in (2,3,4)) and (`t`.`goods_id` = `g`.`id`) and (`t`.`track_id` = `tt`.`id`) and (`a`.`id` = `t`.`area`)) " +
+                    "group by `t`.`goods_id`,`t`.`track_id` " +
+                    "order by `t`.`area`,`t`.`goods_id`,`t`.`produce_time`,`t`.`track_id`";
+                    break;
+                case "inlog":
+                    sql = PubMaster.Mod.GoodSql.GetQueryStockLogSumSql(filterareaid, filterlineid, STARTDATE, STOPDATE);
+                    break;
+
+                case "outlog":
+                    sql = PubMaster.Mod.GoodSql.GetQueryConsumeLogSumList(filterareaid, filterlineid, STARTDATE, STOPDATE);
+                    break;
             }
-            PubMaster.Mod.ExcelConfigSql.SaveToExcel(saveFileDialog, sql);
+
+            if (!string.IsNullOrEmpty(sql))
+            {
+                PubMaster.Mod.ExcelConfigSql.SaveToExcel(saveFileDialog, sql);
+            }
+            else
+            {
+                Growl.Warning("未配置导出语句！");
+            }
+        }
+
+        #endregion
+
+        #region[Tab切换]
+
+        private void TabSelected(RoutedEventArgs orgs)
+        {
+            if (orgs != null && orgs.OriginalSource is HandyControl.Controls.TabControl pro && pro.SelectedItem is HandyControl.Controls.TabItem tabitem)
+            {
+                Now_Tag_Name = tabitem.Tag.ToString();
+            }
         }
 
         #endregion
@@ -367,5 +509,38 @@ namespace wcs.ViewModel
         {
 
         }
+
+        #region[库存查询]
+
+        /// <summary>
+        /// 当前库存查询
+        /// </summary>
+        private void QueryTrackInLog()
+        {
+            List<StockSum>  list = PubMaster.Mod.GoodSql.QueryStockLogSumList(filterareaid, filterlineid, STARTDATE, STOPDATE);
+            TrackStockSearchList.Clear();
+
+            foreach (var item in list)
+            {
+                TrackStockSearchList.Add(new StockSumView(item));
+            }
+        }
+
+        /// <summary>
+        /// 出库库存记录
+        /// </summary>
+        private void QueryTrackOutLog()
+        {
+            List<StockSum> list = PubMaster.Mod.GoodSql.QueryConsumeLogSumList(filterareaid, filterlineid, STARTDATE, STOPDATE);
+            ConsumeStockSearchList.Clear();
+
+            foreach (var item in list)
+            {
+                ConsumeStockSearchList.Add(new StockSumView(item));
+            }
+        }
+
+
+        #endregion
     }
 }
