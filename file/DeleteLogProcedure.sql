f28a94bf502b59322d09248322ac268613e2c848
donggualo <50481112+donggualo@users.noreply.github.com>
2021-08-02 09:06:42 +0800

    2021.08.02：数据库 / 通讯协议

--- file/DeleteLogProcedure.sql
+++ file/DeleteLogProcedure.sql
@@ -5,14 +5,15 @@ DROP PROCEDURE IF EXISTS `DELETE_DATA`;
 delimiter ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DELETE_DATA`()
 BEGIN
-	/*仅保留 31 天的报警数据*/
-	delete from warning where resolve = 1 and DATEDIFF(CURRENT_DATE,createtime) >= 31;
-	/*仅保留 31 天的任务数据*/
-	delete from stock_trans where (finish = 1 or cancel = 1) and DATEDIFF(CURRENT_DATE,create_time) >= 31;
-	/*仅保留 31 天的记录数据*/
-	delete from stock_log where DATEDIFF(CURRENT_DATE,create_time) >= 31;
-	delete from track_log where DATEDIFF(CURRENT_DATE,log_time) >= 31;
-	delete from consume_log where DATEDIFF(CURRENT_DATE,consume_time) >= 31;
+	/*仅保留 90 天的报警数据*/
+	delete from warning where resolve = 1 and DATEDIFF(CURRENT_DATE,createtime) >= 90;
+	/*仅保留 90 天的任务数据*/
+	delete from stock_trans_dtl where dtl_p_id in (select id from stock_trans where (finish = 1 or cancel = 1) and DATEDIFF(CURRENT_DATE,create_time) >= 90);
+	delete from stock_trans where (finish = 1 or cancel = 1) and DATEDIFF(CURRENT_DATE,create_time) >= 90;
+	/*仅保留 90 天的记录数据*/
+	delete from stock_log where DATEDIFF(CURRENT_DATE,create_time) >= 90;
+	delete from track_log where DATEDIFF(CURRENT_DATE,log_time) >= 90;
+	delete from consume_log where DATEDIFF(CURRENT_DATE,consume_time) >= 90;
 	/*仅保留 7 天的交管数据*/
 	delete from traffic_control where DATEDIFF(CURRENT_DATE,create_time) >= 7;
 	/*仅保留 7 天的砖机需求数据*/
@@ -22,7 +23,7 @@ END
 delimiter ;
 
 -- ----------------------------
--- Event structure for DELETE_DATA_EVENT
+-- Event structure for DELETE_DATA_EVENT (开始时间自行修改！！！)
 -- ----------------------------
 DROP EVENT IF EXISTS `DELETE_DATA_EVENT`;
 delimiter ;;
